715e32a5469bf7935f211b65a7c099496c896df2
ifknot <ifknot@hotmail.com>
2025-06-28 15:56:01 +0100

    main.c initial

---
+++ bin/CMakeFiles/chess.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess.dir/main.c.obj"
+  "chess.exe"
+  "chess.lib"
+  "chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/bld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Building for OW2..."
+
+cmake --build ../bin/
