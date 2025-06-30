4ee35dbba06052e2ec742f410596e9a72a967c32
ifknot <ifknot@hotmail.com>
2025-06-30 22:21:40 +0100

    mda context

---
+++ src/MDA/mda_constants.h
@@ -0,0 +1,6 @@
+#ifndef MDA_CONSTANTS_H
+#define MDA_CONSTANTS_H
+
+#define MDA_SCREEN_ROWS     25
+
+#endif
--- src/MDA/mda_context.h
+++ src/MDA/mda_context.h
@@ -7,30 +7,32 @@
 typedef union {
     uint16_t char_attr;
     struct {
-        char chr;     
-        char attr;    
+        char chr;
+        char attr;
     } parts;
 } mda_char_attr_t;
 
-// having different contexts enables manage different parts of the screen in a window-esque way
+// having differentctxs enables manage different parts of the screen in a window-esque way
 typedef struct {
-    char attribute;
+    char attributes;
     char x, y, width, height;
     bios_video_state_t video;
     bios_cursor_state_t cursor;
     // TODO: mouse_state mouse; has mouse support etc
 } mda_context_t;
 
-void mda_initialize_context(mda_context_t* context);
+void mda_initialize_default_context(mda_context_t*ctx);
 
-void mda_cursor_to(mda_context_t* context, uint8_t x, uint8_t y);
+void mda_set_context_frame(mda_context_t*ctx, uint8_t x, uint8_t y, uint8_t width, uint8_t height);
 
-void mda_set_attributes(mda_context_t* context,char attr);
+void mda_cursor_to(mda_context_t*ctx, uint8_t x, uint8_t y);
 
-void mda_reset_attributes(mda_context_t* context);
+void mda_set_attributes(mda_context_t*ctx,char attr);
 
-void mda_print(mda_context_t* context, char* stringz); // teletype print 
+void mda_reset_attributes(mda_context_t*ctx);
 
-bool mda_context_contains(mda_context* context, uint8_t x, uint8_t y); 
+void mda_print(mda_context_t*ctx, char* stringz); // teletype print
+
+bool mda_context_contains(mda_context_t*ctx, uint8_t x, uint8_t y);
 
 #endif
