a8c809199edd4369e852a18462dc812d360c1083
ifknot <ifknot@hotmail.com>
2025-06-30 11:19:17 +0100

    Update mda_context.h

--- src/MDA/mda_context.h
+++ src/MDA/mda_context.h
@@ -1,6 +1,30 @@
 #ifndef MDA_CONTEXT_H
 #define MDA_CONTEXT_H
 
+#include "../BIOS/bios_video_services.h"
 
+typedef union {
+    uint16_t char_attr;
+    struct {
+        char chr;     
+        char attr;    
+    } parts;
+} mda_char_attr_t;
+
+typedef struct {
+    char attribute;
+    bios_video_state_t video;
+    bios_cursor_state_t cursor;
+} mda_context_t;
+
+void mda_initialize_context(mda_context_t* context);
+
+void mda_cursor_to(mda_context_t* context, uint8_t x, uint8_t y);
+
+void mda_set_attributes(mda_context_t* context,char attr);
+
+void mda_reset_attributes(mda_context_t* context);
+
+void mda_print(mda_context_t* context, char* stringz); // teletype print 
 
 #endif
