eb558485d57a836ddbbeb03ff644ba9410401902
ifknot <ifknot@hotmail.com>
2025-07-01 09:14:48 +0100

    Update mda_widgets.h

--- src/MDA/mda_widgets.h
+++ src/MDA/mda_widgets.h
@@ -1,6 +1,10 @@
 #ifndef MDA_WIDGETS_H
 #define MDA_WIDGETS_H
 
+#include <stdbool.h>
+
 // progress bars, spinners, buttons etc
+
+void mda_widget_border(mda_context_t* ctx, char* border_charz, bool shrink_context); // border_charz 6 chars - 4 corners + horz and vert
 
 #endif
