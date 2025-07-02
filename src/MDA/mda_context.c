3c9f9439f743a8b87b493bd505eeb7fb40d717fb
ifknot <ifknot@hotmail.com>
2025-07-02 11:45:46 +0100

    Update mda_context.c

--- src/MDA/mda_context.c
+++ src/MDA/mda_context.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 
 void mda_initialize_default_context(mda_context_t* ctx) {
+    assert(ctx && "NULL context!");
     bios_set_video_mode(MDA_TEXT_MONOCHROME_80X25);
     bios_get_video_state(&ctx->video);
     bios_get_cursor_position_and_size(&ctx->cursor, ctx->video.page);
@@ -12,6 +13,7 @@ void mda_initialize_default_context(mda_context_t* ctx) {
 }
 
 void mda_set_context_frame(mda_context_t*ctx, uint8_t x, uint8_t y, uint8_t width, uint8_t height) {
+    assert(ctx && "NULL context!");
     assert(x < ctx->video.columns && "OUT OF RANGE x value!");
     assert(y < MDA_SCREEN_ROWS && "OUT OF RANGE y value!");
     ctx->x = x;
@@ -21,19 +23,26 @@ void mda_set_context_frame(mda_context_t*ctx, uint8_t x, uint8_t y, uint8_t widt
 }
 
 void mda_cursor_to(mda_context_t*ctx, uint8_t x, uint8_t y) {
+    assert(ctx && "NULL context!");
     assert(mda_context_contains(ctx, x, y) && "OUT OF BOUNDS cursor position!");
     bios_set_cursor_position(x, y, ctx->video.page);
 }
 
 void mda_set_attributes(mda_context_t*ctx,char attr) {
+    assert(ctx && "NULL context!");
     ctx->attributes = attr;
 }
 
 void mda_reset_attributes(mda_context_t*ctx) {
+    assert(ctx && "NULL context!");
     ctx->attributes = MDA_NORMAL;
 }
 
 void mda_print_char(mda_context_t ctx*, char chr, uint16_t count) {
+    assert(ctx && "NULL context!");
+    if(!count) {
+        return;
+    }
     bios_write_character_and_attribute_at_cursor(stringz[i++], ctx->attributes, 1, ctx->video.page); 
     for(int i = 0; i < count; ++i) {
         ctx->x++;
@@ -47,6 +56,8 @@ void mda_print_char(mda_context_t ctx*, char chr, uint16_t count) {
 
 
 void mda_print_string(mda_context_t*ctx, char* stringz) {
+    assert(ctx && "NULL context!");
+    assert(stringz && "NULL string!");
     int i = 0;
     while(stringz[i]) {
         mda_print_char(stringz[i++]);
@@ -54,5 +65,8 @@ void mda_print_string(mda_context_t*ctx, char* stringz) {
 }
 
 bool mda_context_contains(mda_context_t*ctx, uint8_t x, uint8_t y) {
+    assert(ctx && "NULL context!");
+    assert(x < ctx->video.columns && "OUT OF RANGE x value!");
+    assert(y < MDA_SCREEN_ROWS && "OUT OF RANGE y value!");
     return x >=ctx->x && x < ctx->width && y >= ctx->y && y < ctx->height;
 }
