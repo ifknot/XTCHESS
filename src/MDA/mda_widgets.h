a0870bfb51ef03978cb00e71f18128f20a90d5bc
ifknot <ifknot@hotmail.com>
2025-07-02 16:19:43 +0100

    Update mda_widgets.h

--- src/MDA/mda_widgets.h
+++ src/MDA/mda_widgets.h
@@ -27,6 +27,6 @@ typedef enum {
 // mda_widget_composite 
 // mda_widget_rtti
 
-void mda_widget_border(mda_context_t* ctx, char* border_charz, bool shrink_context); // border_charz 6 chars - 4 corners start top lhs then clockwise then horz and vert
+void mda_widget_draw_border(mda_context_t* ctx, char* border_charz, bool shrink_context); // border_charz 6 chars - 4 corners start top lhs then clockwise then horz and vert
 
 #endif
