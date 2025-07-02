12504067bc77320933d004a27b2bc6e5de59b8cc
ifknot <ifknot@hotmail.com>
2025-07-02 15:54:57 +0100

    Update mda_widgets.c

--- src/MDA/mda_widgets.c
+++ src/MDA/mda_widgets.c
@@ -3,15 +3,17 @@
 
 void mda_widget_border(mda_context_t* ctx, char* border_charz, bool shrink_context) {
   assert(border_charz && "NULL border characters");
+  uint8_t x = ctx->x;
+  uint8_t y = ctx->y;
   mda_print_char(ctx, border_charz[INDEX_TOP_LEFT]);
-  mda_print_row(ctx, border_charz[INDEX_HORIZONTAL, ctx.width - 2);
+  mda_print_row(ctx, border_charz[INDEX_HORIZONTAL, ctx->width - 2);
   mda_print_char(ctx, border_charz[INDEX_TOP_RIGHT]);
-  mda_cursor_to(ctx, ctx.x, ctx.height - 1);  
+  mda_cursor_to(ctx, x, y+1);
+  mda_print_column(ctx, border_charz[INDEX_VERTICAL, ctx->height - 2);  
   mda_print_char(ctx, border_charz[INDEX_BOTTOM_LEFT]);
   mda_print_row(ctx, border_charz[INDEX_HORIZONTAL], ctx.width - 2);
   mda_print_char(ctx, border_charz[INDEX_BOTTOM_RIGHT]);
-  // move 
-  // column
-  // move 
-  // column
+  mda_cursor_to(ctx, x + ctx->width - 1, y);
+  mda_print_column(ctx, border_charz[INDEX_VERTICAL, ctx->height - 2);  
+ 
 }
