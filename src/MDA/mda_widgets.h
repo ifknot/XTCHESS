42a323cef48f104a48cc04fe81f09bc6dd7cbbc5
ifknot <ifknot@hotmail.com>
2025-07-02 10:56:57 +0100

    Update mda_widgets.h

--- src/MDA/mda_widgets.h
+++ src/MDA/mda_widgets.h
@@ -5,6 +5,6 @@
 
 // progress bars, spinners, buttons etc
 
-void mda_widget_border(mda_context_t* ctx, char* border_charz, bool shrink_context); // border_charz 6 chars - 4 corners + horz and vert
+void mda_widget_border(mda_context_t* ctx, char* border_charz, bool shrink_context); // border_charz 6 chars - 4 corners start top lhs then clockwise then horz and vert
 
 #endif
