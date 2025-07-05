769b2ec1870b1ed3699bb4962d838a3a933efb0b
ifknot <ifknot@hotmail.com>
2025-07-05 18:55:40 +0100

    update cmkafiles

--- src/cmk.sh
+++ src/cmk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Running CMAKE for OW2..."
 
+# clean cache
+rm -rf ../bin/CMakeCache.txt ../bin/CMakeFiles/
+
 cmake -G "Watcom WMake" -D CMAKE_SYSTEM_NAME=DOS -D CMAKE_SYSTEM_PROCESSOR=I86 -S. -B ../bin
