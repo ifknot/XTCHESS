79470b405a5edcb2279728f28c00501cfc74ccb3
ifknot <ifknot@hotmail.com>
2025-07-31 22:07:30 +0100

    shells

---
+++ login_ifknot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Kitchen sink login for if knot..."
+ssh -T git@github.com-kitchen
---
+++ setup_ifknot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Setting up kitchen sink for ifknot..."
+git config user.name "ifknot"
+git config user.email "ifknot@hotmail.com"
+git config user.name
+git config user.email
+ssh -T git@github.com-kitchen
