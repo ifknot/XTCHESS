b832b079ad64c19e0f54a691ae77192c78b480d9
ifknot <ifknot@hotmail.com>
2025-08-02 21:16:47 +0100

    Update login_ifknot.sh

--- login_ifknot.sh
+++ login_ifknot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Kitchen sink login for if knot..."
-ssh -T git@github.com-kitchen
+echo "SSH kitchen key for ifknot..."
+ssh-add -D #strip old one(s) because Sonoma updated f**ked it all
+ssh-add ~/.ssh/id_ed25519_kitchen
